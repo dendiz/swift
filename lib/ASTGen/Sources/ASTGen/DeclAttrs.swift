dd89bb7ef6b369292ffe422f9365a2f02ab898d9
Becca Royal-Gordon <beccadax@apple.com>
2025-05-08 21:04:27 -0700

    [NFC] Add DeclNameLoc to specialize/dynamicReplacement

--- lib/ASTGen/Sources/ASTGen/DeclAttrs.swift
+++ lib/ASTGen/Sources/ASTGen/DeclAttrs.swift
@@ -775,6 +775,7 @@ extension ASTGenVisitor {
       attrNameLoc: self.generateSourceLoc(node.attributeName),
       lParenLoc: self.generateSourceLoc(node.leftParen),
       replacedFunction: replacedFunction.name,
+      replacedFunctionLoc: replacedFunction.loc,
       rParenLoc: self.generateSourceLoc(node.rightParen)
     )
   }
@@ -1918,7 +1919,7 @@ extension ASTGenVisitor {
     let exported: Bool? = nil
     let kind: BridgedSpecializationKind? = nil
     var whereClause: BridgedTrailingWhereClause? = nil
-    let targetFunction: BridgedDeclNameRef? = nil
+    let targetFunction: (name: BridgedDeclNameRef, loc: BridgedDeclNameLoc)? = nil
     let spiGroups: [Identifier] = []
     let availableAttrs: [BridgedAvailableAttr] = []
 
@@ -1931,7 +1932,8 @@ extension ASTGenVisitor {
       whereClause: whereClause.asNullable,
       exported: exported ?? false,
       kind: kind ?? .full,
-      taretFunction: targetFunction ?? BridgedDeclNameRef(),
+      targetFunction: targetFunction?.name ?? BridgedDeclNameRef(),
+      targetFunctionLoc: targetFunction?.loc ?? BridgedDeclNameLoc(),
       spiGroups: spiGroups.lazy.bridgedArray(in: self),
       availableAttrs: availableAttrs.lazy.bridgedArray(in: self)
     )
@@ -1951,7 +1953,7 @@ extension ASTGenVisitor {
     var exported: Bool?
     var kind: BridgedSpecializationKind? = nil
     var whereClause: BridgedTrailingWhereClause? = nil
-    var targetFunction: BridgedDeclNameRef? = nil
+    var targetFunction: (name: BridgedDeclNameRef, loc: BridgedDeclNameLoc)?
     var spiGroups: [Identifier] = []
     var availableAttrs: [BridgedAvailableAttr] = []
 
@@ -1963,7 +1965,7 @@ extension ASTGenVisitor {
         if targetFunction != nil {
           // TODO: Diangose.
         }
-        targetFunction = self.generateDeclNameRef(declReferenceExpr: arg.declName).name
+        targetFunction = self.generateDeclNameRef(declReferenceExpr: arg.declName)
       case .specializeAvailabilityArgument(let arg):
         availableAttrs = self.generateAvailableAttr(
           atLoc: self.generateSourceLoc(arg.availabilityLabel),
@@ -2027,7 +2029,8 @@ extension ASTGenVisitor {
       whereClause: whereClause.asNullable,
       exported: exported ?? false,
       kind: kind ?? .full,
-      taretFunction: targetFunction ?? BridgedDeclNameRef(),
+      targetFunction: targetFunction?.name ?? BridgedDeclNameRef(),
+      targetFunctionLoc: targetFunction?.loc ?? BridgedDeclNameLoc(),
       spiGroups: spiGroups.lazy.bridgedArray(in: self),
       availableAttrs: availableAttrs.lazy.bridgedArray(in: self)
     )
