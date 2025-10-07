b93a185fa5c7afb994bbd2a7a027ab337381ae55
Allan Shortlidge <shortlidge@apple.com>
2025-10-07 09:50:39 -0700

    AST: Introduce the SwiftLanguageMode availability domain spelling.
    Require `-enable-experimental-feature SwiftRuntimeAvailability` to use this new
    spelling.

--- lib/Serialization/ModuleFormat.h
+++ lib/Serialization/ModuleFormat.h
@@ -780,7 +780,7 @@ using GenericParamKindField = BCFixed<2>;
 // the module version.
 enum class AvailabilityDomainKind : uint8_t {
   Universal = 0,
-  SwiftLanguage,
+  SwiftLanguageMode,
   PackageDescription,
   Embedded,
   Platform,
